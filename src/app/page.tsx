95c1ec10175bb24ea2c8586abd0e185f5c6884e7
ShSato4JPN <shsato4.private.1221@gmail.com>
2026-02-22 23:52:35 +0900

    add flex-n source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,26 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="w-full bg-gray-300 p-4 rounded-lg">
+        <h1>flex-n 確認</h1>
+        <div className="flex gap-4 bg-blue-400">
+          {/* <div className="shrink-0 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-0</span>
+          </div> */}
+          <div className="flex-1 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-1</span>
+          </div>
+          <div className="flex-2 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-2</span>
+          </div>
+          <div className="flex-3 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-3</span>
+          </div>
+          <div className="flex-4 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-4</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
