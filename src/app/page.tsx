6bf7b738f2fc74fc1549d66db4a4d34adecb05ad
ShSato4JPN <shsato4.private.1221@gmail.com>
2026-02-22 23:51:37 +0900

    add flex shrink source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,39 @@ function GrowItem({ children }: { children: React.ReactNode }) {
   );
 }
 
-
 export default function Home() {
   return (
     <div className="w-dvw h-dvh bg-orange-200 grid place-items-center">
-      <h1>flex grow 確認</h1>
-      <div className="w-full flex gap-4">
-        <Item>
-          <span>itemA</span>
-        </Item>
-        <GrowItem>
-          <span>itemA</span>
-        </GrowItem>
+      <div className="w-full bg-gray-300 p-4 rounded-lg">
+        <h1>flex grow 確認</h1>
+        <div className="w-full flex gap-4">
+          <Item>
+            <span>itemA</span>
+          </Item>
+          <GrowItem>
+            <span>itemA</span>
+          </GrowItem>
+        </div>
+      </div>
+      <div className="w-full bg-gray-300 p-4 rounded-lg">
+        <h1>flex shrink 確認</h1>
+        <div className="flex gap-4 bg-blue-400">
+          {/* <div className="shrink-0 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-0</span>
+          </div> */}
+          <div className="shrink-1 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-1</span>
+          </div>
+          <div className="shrink-2 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-2</span>
+          </div>
+          <div className="shrink-3 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-3</span>
+          </div>
+          <div className="shrink-3 w-full p-4 border rounded-lg bg-white">
+            <span>shrink-3</span>
+          </div>
+        </div>
       </div>
     </div>
   );
