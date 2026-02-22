48c04bdbf045e670413b80dc7a1564518c57ef8d
ShSato4JPN <shsato4.private.1221@gmail.com>
2026-02-22 23:36:11 +0900

    add sample source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { ModeToggle } from "@/components/mode-toggle";
+function Item({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-[500px] border rounded-lg p-10 bg-white">{children}</div>
+  );
+}
+
+function GrowItem({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grow w-[500px] border rounded-lg p-10 bg-white">
+      {children}
+    </div>
+  );
+}
+
 
 export default function Home() {
   return (
-    <div>
-      <div className="bg-test">test message</div>
-      <ModeToggle />
+    <div className="w-dvw h-dvh bg-orange-200 grid place-items-center">
+      <h1>flex grow 確認</h1>
+      <div className="w-full flex gap-4">
+        <Item>
+          <span>itemA</span>
+        </Item>
+        <GrowItem>
+          <span>itemA</span>
+        </GrowItem>
+      </div>
     </div>
   );
 }
